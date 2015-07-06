bf2533d2fb9b3ee5587e82c9b484d0fe04e55977
Bruno Lebtag <brlebtag@gmail.com>
2015-07-06 14:00:58 -0300

    Movendo arquivos AuthController para a pasta App/Http/Controllers/Auth.

--- En/Project-ReCAPTCHARegister/app/Http/Controllers/Auth/AuthController.php
+++ En/Project-ReCAPTCHARegister/app/Http/Controllers/Auth/AuthController.php
@@ -15,7 +15,7 @@ class AuthController extends Controller
 	public function __construct()
 	{
 		$this->middleware('guest', ['except' => 'getLogout']);
-		$this->middleware('recaptcha');
+		$this->middleware('recaptcha', ['only' => 'postRegister']);
 	}
 
 	public function validator(array $data)
